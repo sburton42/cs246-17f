c5cb9d4ca7c5885c81d07625b3c87977780c2f5a
Scott Burton <scotthburton@gmail.com>
2017-09-29 15:02:37 -0600

    Class example from the 2:00 section

--- collections/src/edu/byui/cs246/Main.java
+++ collections/src/edu/byui/cs246/Main.java
@@ -1,8 +1,56 @@
 package edu.byui.cs246;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+
+        List<String> books = new ArrayList<>();
+
+        Map<String, Integer> map = new TreeMap<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("booksAndChapters.txt"))) {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+
+                String[] parts = line.split(":");
+                String book = parts[0];
+                int chapters = Integer.parseInt(parts[1]);
+
+                map.put(book, chapters);
+
+
+                books.add(line);
+            }
+
+        }
+
+        for (String key : map.keySet()) {
+            int value = map.get(key);
+
+            System.out.println(key);
+            System.out.println(value);
+
+        }
+
+        System.out.format("There are %d books in the standard works%n", books.size());
+
+        //displayList(books);
+    }
+
+    public static void displayList(List<String> books) {
+
+        for (String book : books) {
+            System.out.println(book);
+        }
     }
 }
